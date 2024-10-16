64785660493cbe08d7f16f5e26fe8140a101d0de
Xandra Campo <xkmpera@gmail.com>
2024-10-16 17:34:51 +0200

    Add availability of different interpolation methods

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import math
-from scipy.interpolate import Akima1DInterpolator
+from scipy.interpolate import CubicSpline, PchipInterpolator, Akima1DInterpolator
 
 
 class Spectrum:
@@ -14,18 +14,38 @@ class Spectrum:
         self.log_energy = [math.log(e) for e in self.energy]
         self.log_values = [math.log(v) for v in self.values]
 
-    def interpolate(self, new_energies, log_scale=False):
+    def interpolate(self, new_energies, log_scale=False, method='Akima1D'):
         """Interpolates the spectrum to a new set of energy values."""
         if log_scale:
             # Case 2: interpolate using log scale for the spectrum and new energies using akima
             self.apply_log_transform()
             log_new_energies = [math.log(e) for e in new_energies]
-            interpolator = Akima1DInterpolator(self.log_energy, self.log_values)
+
+            # https://docs.scipy.org/doc/scipy/tutorial/interpolate.html
+            if method=='CubicSpline':
+                interpolator = CubicSpline(self.log_energy, self.log_values)
+            elif method=='PchipInterpolator':
+                interpolator = PchipInterpolator(self.log_energy, self.log_values)
+            elif method=='Akima1D':
+                interpolator = Akima1DInterpolator(self.log_energy, self.log_values)
+            else:
+                raise ValueError('Interpolation methods: CubicSpline, PchipInterpolator and Akima1D')
+
             log_interpolated_values = interpolator(log_new_energies)
+
             interpolated_values = [math.exp(v) for v in log_interpolated_values]
         else:
             # Case 1: interpolate using linear scale for the spectrum and new energies using akima
-            interpolator = Akima1DInterpolator(self.energy, self.values)
+            # https://docs.scipy.org/doc/scipy/tutorial/interpolate.html
+            if method == 'CubicSpline':
+                interpolator = CubicSpline(self.energy, self.values)
+            elif method == 'PchipInterpolator':
+                interpolator = PchipInterpolator(self.energy, self.values)
+            elif method == 'Akima1D':
+                interpolator = Akima1DInterpolator(self.energy, self.values)
+            else:
+                raise ValueError('Interpolation methods: PchipInterpolator CubicSpline, and Akima1D')
+
             interpolated_values = interpolator(new_energies)
 
         return Spectrum(new_energies, interpolated_values)
@@ -38,23 +58,35 @@ class Spectrum:
 
 def main():
     # Input data
-    fluence_energies, fluence_values = [1, 2, 3], [10, 20, 30]
-    print(f'Fluence input data: {fluence_energies}, {fluence_values}')
+    energies, fluences = [1, 2, 3], [10, 20, 30]
+    print(f'Input data: {energies}, {fluences}')
 
     # Create Spectrum object
-    fluence_spectrum = Spectrum(fluence_energies, fluence_values)
-    print(f'Fluence spectrum (lin scale): {fluence_spectrum.energy}, {fluence_spectrum.values}')
+    spectrum = Spectrum(energies, fluences)
+    print(f'Spectrum (lin scale): {spectrum.energy}, {spectrum.values}')
 
     # Apply logarithmic transformation
-    fluence_spectrum.apply_log_transform()
-    print(f'Fluence spectrum (log scale): {fluence_spectrum.log_energy}, {fluence_spectrum.log_values}')
-    print(f'Fluence spectrum (log scale): {[math.log(i) for i in fluence_energies]}, {[math.log(i) for i in fluence_values]}')
+    spectrum.apply_log_transform()
+    print(f'Spectrum (log scale): {spectrum.log_energy}, {spectrum.log_values}')
+    print(f'Spectrum (log scale): {[math.log(i) for i in energies]}, {[math.log(i) for i in fluences]}')
 
-    interpolated_fluence_spectrum = fluence_spectrum.interpolate([1, 1.5, 2, 2.5, 3])
-    print(f'Interpolated fluence spectrum (lin interpolation): {interpolated_fluence_spectrum.energy}, {interpolated_fluence_spectrum.values}')
+    # Interpolate in linear scale
+    interpolated_spectrum = spectrum.interpolate([1, 1.5, 2, 2.5, 3], log_scale=False)
+    print(f'Interpolated spectrum (lin interpolation): {interpolated_spectrum.energy}, {interpolated_spectrum.values}')
+    # Interpolate in logarithmic scale
+    interpolated_spectrum = spectrum.interpolate([1, 1.5, 2, 2.5, 3], log_scale=True)
+    print(f'Interpolated spectrum (log interpolation): {interpolated_spectrum.energy}, {interpolated_spectrum.values}')
 
-    interpolated_fluence_spectrum = fluence_spectrum.interpolate([1, 1.5, 2, 2.5, 3], log_scale=True)
-    print(f'Interpolated fluence spectrum (log interpolation): {interpolated_fluence_spectrum.energy}, {interpolated_fluence_spectrum.values}')
+    # Interpolate in logarithmic scale using CubicSplines
+    interpolated_spectrum = spectrum.interpolate([1, 1.5, 2, 2.5, 3], log_scale=True, method='CubicSpline')
+    print(f'Interpolated spectrum (CubicSplines): {interpolated_spectrum.energy}, {interpolated_spectrum.values}')
+    # Interpolate in logarithmic scale using PchipInterpolator
+    interpolated_spectrum = spectrum.interpolate([1, 1.5, 2, 2.5, 3], log_scale=True, method='PchipInterpolator')
+    print(f'Interpolated spectrum (PchipInterpolator): {interpolated_spectrum.energy}, {interpolated_spectrum.values}')
+    # Interpolate in logarithmic scale using Akima1D
+    interpolated_spectrum = spectrum.interpolate([1, 1.5, 2, 2.5, 3], log_scale=True, method='Akima1D')
+    print(f'Interpolated spectrum (Akima1D): {interpolated_spectrum.energy}, {interpolated_spectrum.values}')
+
 
 if __name__ == "__main__":
     main()
