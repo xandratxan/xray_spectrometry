e9444fd3d1c83e5a080da29e905c388f913bd1de
Xandra Campo <xkmpera@gmail.com>
2024-10-16 17:09:31 +0200

    Add method to interpolate a spectrum using linear or logarithmic scale

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import math
+from scipy.interpolate import Akima1DInterpolator
 
 
 class Spectrum:
@@ -13,10 +14,21 @@ class Spectrum:
         self.log_energy = [math.log(e) for e in self.energy]
         self.log_values = [math.log(v) for v in self.values]
 
-    def interpolate(self, new_energy):
+    def interpolate(self, new_energies, log_scale=False):
         """Interpolates the spectrum to a new set of energy values."""
-        interpolated_values = None
-        return interpolated_values
+        if log_scale:
+            # Case 2: interpolate using log scale for the spectrum and new energies using akima
+            self.apply_log_transform()
+            log_new_energies = [math.log(e) for e in new_energies]
+            interpolator = Akima1DInterpolator(self.log_energy, self.log_values)
+            log_interpolated_values = interpolator(log_new_energies)
+            interpolated_values = [math.exp(v) for v in log_interpolated_values]
+        else:
+            # Case 1: interpolate using linear scale for the spectrum and new energies using akima
+            interpolator = Akima1DInterpolator(self.energy, self.values)
+            interpolated_values = interpolator(new_energies)
+
+        return Spectrum(new_energies, interpolated_values)
 
     def calculate_hvl(self):
         """Calculates the Half-Value Layer (HVL) for the spectrum."""
@@ -38,6 +50,11 @@ def main():
     print(f'Fluence spectrum (log scale): {fluence_spectrum.log_energy}, {fluence_spectrum.log_values}')
     print(f'Fluence spectrum (log scale): {[math.log(i) for i in fluence_energies]}, {[math.log(i) for i in fluence_values]}')
 
+    interpolated_fluence_spectrum = fluence_spectrum.interpolate([1, 1.5, 2, 2.5, 3])
+    print(f'Interpolated fluence spectrum (lin interpolation): {interpolated_fluence_spectrum.energy}, {interpolated_fluence_spectrum.values}')
+
+    interpolated_fluence_spectrum = fluence_spectrum.interpolate([1, 1.5, 2, 2.5, 3], log_scale=True)
+    print(f'Interpolated fluence spectrum (log interpolation): {interpolated_fluence_spectrum.energy}, {interpolated_fluence_spectrum.values}')
 
 if __name__ == "__main__":
     main()
