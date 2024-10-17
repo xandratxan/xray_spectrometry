8e2d2f8b423bb84462334fe5dec8fc6170dd225f
Xandra Campo <xkmpera@gmail.com>
2024-10-17 18:16:44 +0200

    Add script to get familiar with scipy interpolation techniques for one-dimensional data

---
+++ dev/scipy_interpolation.py
@@ -0,0 +1,14 @@
+# Script to get familiar with scipy interpolation techniques for one-dimensional data
+# References
+# https://docs.scipy.org/doc/scipy/tutorial/interpolate.html
+# https://docs.scipy.org/doc/scipy/tutorial/interpolate/1D.html
+
+# Piecewise linear interpolation
+
+# Cubic splines
+
+# Monotone interpolants
+
+# Interpolation with B-splines
+
+# Parametric spline curves
