283b5e969697005abf1a2372d03bd41d89ba4e78
Xandra Campo <xkmpera@gmail.com>
2024-10-24 14:30:08 +0200

    Add cubic splines interpolation example

--- dev/scipy_interpolation.py
+++ dev/scipy_interpolation.py
@@ -4,34 +4,32 @@
 # https://docs.scipy.org/doc/scipy/tutorial/interpolate/1D.html
 
 import numpy as np
+from scipy.interpolate import CubicSpline
 import matplotlib.pyplot as plt
-
-# Piecewise linear interpolation
 
 # Define data to interpolate
 # - Two arrays of data to interpolate, x, and y
 # - A third array, xnew, of points to evaluate the interpolation on
 x = np.linspace(0, 10, num=11)
 y = np.cos(-x ** 2 / 9.0)
-x_new = np.linspace(0, 10, num=21)
-print(x)
-print(y)
-print(x_new)
+x_new = np.linspace(0, 10, num=101)
 
-# Interpolate
-y_new = np.interp(x_new, x, y)
-print(y_new)
-
-# Plot the results
-plt.plot(x_new, y_new, '-', label='Linear')
-plt.plot(x, y, 'o', label='Data')
-plt.legend(loc='best')
-plt.show()
+# Piecewise linear interpolation
+y_new_linear = np.interp(x_new, x, y)
 
 # Cubic splines
+interpolator_splines = CubicSpline(x, y)
+y_new_splines = interpolator_splines(x_new)
 
 # Monotone interpolants
 
 # Interpolation with B-splines
 
 # Parametric spline curves
+
+# Plot the results
+plt.plot(x, y, 'o', label='Data')
+plt.plot(x_new, y_new_linear, '-', label='Piecewise linear')
+plt.plot(x_new, y_new_splines, '-', label='Cubic splines')
+plt.legend(loc='best')
+plt.show()
