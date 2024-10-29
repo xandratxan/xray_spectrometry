5d569bf53d42d748c1989c74c5b666f6304996ca
Xandra Campo <xkmpera@gmail.com>
2024-10-29 12:07:40 +0100

    Add test for interpolation methods

--- tests/test_interpolator.py
+++ tests/test_interpolator.py
@@ -4,63 +4,65 @@ from tempfile import NamedTemporaryFile
 import numpy as np
 import pandas as pd
 import pytest
+from scipy.interpolate import CubicSpline, PchipInterpolator, Akima1DInterpolator, make_interp_spline
 
 from src.spectrometry.interpolator import Interpolator, read_file
 
 
 class TestInterpolator:
-    class TestInitialization:
-        def test_valid_x_y(self):
-            interpolator = Interpolator(x=[1, 2, 3], y=[4, 5, 6])
-            assert np.array_equal(interpolator.x, np.array([1, 2, 3]))
-            assert np.array_equal(interpolator.y, np.array([4, 5, 6]))
+    class TestConstructor:
+        class TestInitialization:
+            def test_valid_x_y(self):
+                interpolator = Interpolator(x=[1, 2, 3], y=[4, 5, 6])
+                assert np.array_equal(interpolator.x, np.array([1, 2, 3]))
+                assert np.array_equal(interpolator.y, np.array([4, 5, 6]))
 
-        def test_valid_data_dict(self):
-            data = {'x': [1, 2, 3], 'y': [4, 5, 6]}
-            interpolator = Interpolator(data=data)
-            assert np.array_equal(interpolator.x, np.array([1, 2, 3]))
-            assert np.array_equal(interpolator.y, np.array([4, 5, 6]))
+            def test_valid_data_dict(self):
+                data = {'x': [1, 2, 3], 'y': [4, 5, 6]}
+                interpolator = Interpolator(data=data)
+                assert np.array_equal(interpolator.x, np.array([1, 2, 3]))
+                assert np.array_equal(interpolator.y, np.array([4, 5, 6]))
 
-        def test_valid_data_dataframe(self):
-            data = pd.DataFrame({'x': [1, 2, 3], 'y': [4, 5, 6]})
-            interpolator = Interpolator(data=data)
-            assert np.array_equal(interpolator.x, np.array([1, 2, 3]))
-            assert np.array_equal(interpolator.y, np.array([4, 5, 6]))
+            def test_valid_data_dataframe(self):
+                data = pd.DataFrame({'x': [1, 2, 3], 'y': [4, 5, 6]})
+                interpolator = Interpolator(data=data)
+                assert np.array_equal(interpolator.x, np.array([1, 2, 3]))
+                assert np.array_equal(interpolator.y, np.array([4, 5, 6]))
 
-        def test_invalid_both_x_y_and_data(self):
-            with pytest.raises(ValueError, match="Provide either 'x' and 'y' or 'data'"):
-                Interpolator(x=[1, 2, 3], y=[4, 5, 6], data=[(1, 4), (2, 5), (3, 6)])
+            def test_invalid_both_x_y_and_data(self):
+                with pytest.raises(ValueError, match="Provide either 'x' and 'y' or 'data'"):
+                    Interpolator(x=[1, 2, 3], y=[4, 5, 6], data=[(1, 4), (2, 5), (3, 6)])
 
-        def test_invalid_neither_x_y_nor_data(self):
-            with pytest.raises(ValueError, match="Provide either 'x' and 'y' or 'data'"):
-                Interpolator()
+            def test_invalid_neither_x_y_nor_data(self):
+                with pytest.raises(ValueError, match="Provide either 'x' and 'y' or 'data'"):
+                    Interpolator()
 
-    class TestArgumentTypes:
-        def test_invalid_x_type(self):
-            with pytest.raises(ValueError, match="Arguments must be non-string iterables"):
-                Interpolator(x="invalid", y=[4, 5, 6])
+        class TestArgumentTypes:
+            def test_invalid_x_type(self):
+                with pytest.raises(ValueError, match="Arguments must be non-string iterables"):
+                    Interpolator(x="invalid", y=[4, 5, 6])
 
-        def test_invalid_y_type(self):
-            with pytest.raises(ValueError, match="Arguments must be non-string iterables"):
-                Interpolator(x=[1, 2, 3], y="invalid")
+            def test_invalid_y_type(self):
+                with pytest.raises(ValueError, match="Arguments must be non-string iterables"):
+                    Interpolator(x=[1, 2, 3], y="invalid")
 
-        def test_invalid_data_type(self):
-            with pytest.raises(ValueError, match="Arguments must be non-string iterables"):
-                Interpolator(data="invalid")
+            def test_invalid_data_type(self):
+                with pytest.raises(ValueError, match="Arguments must be non-string iterables"):
+                    Interpolator(data="invalid")
 
-    class TestElementTypes:
-        def test_invalid_x_element_type(self):
-            with pytest.raises(ValueError, match="Elements of arguments must be numerical"):
-                Interpolator(x=[1, 2, "invalid"], y=[4, 5, 6])
+        class TestElementTypes:
+            def test_invalid_x_element_type(self):
+                with pytest.raises(ValueError, match="Elements of arguments must be numerical"):
+                    Interpolator(x=[1, 2, "invalid"], y=[4, 5, 6])
 
-        def test_invalid_y_element_type(self):
-            with pytest.raises(ValueError, match="Elements of arguments must be numerical"):
-                Interpolator(x=[1, 2, 3], y=[4, 5, "invalid"])
+            def test_invalid_y_element_type(self):
+                with pytest.raises(ValueError, match="Elements of arguments must be numerical"):
+                    Interpolator(x=[1, 2, 3], y=[4, 5, "invalid"])
 
-        def test_invalid_data_element_type(self):
-            data = {'x': [1, 2, 3], 'y': [4, 5, "invalid"]}
-            with pytest.raises(ValueError, match="Elements of arguments must be numerical"):
-                Interpolator(data=data)
+            def test_invalid_data_element_type(self):
+                data = {'x': [1, 2, 3], 'y': [4, 5, "invalid"]}
+                with pytest.raises(ValueError, match="Elements of arguments must be numerical"):
+                    Interpolator(data=data)
 
     class TestStringRepresentations:
         def test_repr(self):
@@ -70,6 +72,46 @@ class TestInterpolator:
         def test_str(self):
             interpolator = Interpolator(x=[1, 2, 3], y=[4, 5, 6])
             assert str(interpolator) == "Interpolator with:\nx: [1 2 3]\ny: [4 5 6]"
+
+    class TestInterpolate:
+        def setup_method(self):
+            self.x = np.array([1, 2, 3])
+            self.y = np.array([10, 20, 30])
+            self.new_x = 1.5
+            self.interpolator = Interpolator(x=self.x, y=self.y)
+
+        def test_piecewise_linear(self):
+            expected_y = np.interp(self.new_x, self.x, self.y)
+            result_y = self.interpolator.interpolate(self.new_x, method='PiecewiseLinear')
+            assert result_y == expected_y, f"Expected {expected_y}, but got {result_y}"
+
+        def test_cubic_spline(self):
+            interpolator = CubicSpline(self.x, self.y)
+            expected_y = interpolator(self.new_x)
+            result_y = self.interpolator.interpolate(self.new_x, method='CubicSpline')
+            assert result_y == expected_y, f"Expected {expected_y}, but got {result_y}"
+
+        def test_pchip(self):
+            interpolator = PchipInterpolator(self.x, self.y)
+            expected_y = interpolator(self.new_x)
+            result_y = self.interpolator.interpolate(self.new_x, method='Pchip')
+            assert result_y == expected_y, f"Expected {expected_y}, but got {result_y}"
+
+        def test_akima1d(self):
+            interpolator = Akima1DInterpolator(self.x, self.y)
+            expected_y = interpolator(self.new_x)
+            result_y = self.interpolator.interpolate(self.new_x, method='Akima1D')
+            assert result_y == expected_y, f"Expected {expected_y}, but got {result_y}"
+
+        def test_b_splines(self):
+            interpolator = make_interp_spline(self.x, self.y, k=2)
+            expected_y = interpolator(self.new_x)
+            result_y = self.interpolator.interpolate(self.new_x, method='B-splines')
+            assert result_y == expected_y, f"Expected {expected_y}, but got {result_y}"
+
+        def test_invalid_method(self):
+            with pytest.raises(ValueError, match="Interpolation methods: PiecewiseLinear, CubicSpline, Pchip, Akima1D, B-splines"):
+                self.interpolator.interpolate(self.new_x, method='InvalidMethod')
 
 
 class TestReadFile:
@@ -124,12 +166,11 @@ class TestReadFile:
                 with pytest.raises(ValueError, match="Specified columns are not found in the file."):
                     read_file(tmp.name, x_col=2, y_col=3)
 
-    class TestInvalidFileType:
+    class TestInvalidFile:
         def test_invalid_file_type(self):
             with pytest.raises(ValueError, match="Unsupported file type. Must be a CSV or Excel file."):
                 read_file('data.txt')
 
-    class TestErrorHandling:
         def test_file_not_found(self):
             with pytest.raises(ValueError, match="Error reading file:"):
                 read_file('non_existent_file.csv')
